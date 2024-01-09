fc059c7f123729078876870f1aa892949e2b24b2
RafalPujer <rafal.pujer00@gmail.com>
2024-01-09 22:00:14 +0100

    added flat shading

--- SimpleSoftwareRenderer/Program.cs
+++ SimpleSoftwareRenderer/Program.cs
@@ -2,8 +2,6 @@
 using SimpleSoftwareRenderer;
 using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.X86;
 
 Console.WriteLine("Hello, World!");
 
@@ -28,7 +26,8 @@ while (!window.Quit)
     //LinesTest();
     //ProjectionTest();
     //BackFaceTest();
-    ClipingTest();
+    //ClipingTest();
+    FlatShadingTest();
 
     window.Draw(pixels);
 
@@ -36,6 +35,111 @@ while (!window.Quit)
     Console.WriteLine($"FPS: {oneSecond / stopwatch.Elapsed}");
     deltaTime = stopwatch.ElapsedMilliseconds;
     stopwatch.Restart();
+}
+
+void FlatShadingTest()
+{
+    const int pointCount = 8;
+
+    var vertices = new Vector4[pointCount]
+    {
+        new( 0.75f, 0.75f, 0.75f, 1.0f),
+        new( -0.75f, 0.75f, 0.75f, 1.0f),
+        new( -0.75f, -0.75f, 0.75f, 1.0f),
+        new( 0.75f, -0.75f, 0.75f, 1.0f),
+
+        new( -0.75f, 0.75f, -0.75f, 1.0f),
+        new( 0.75f, 0.75f, -0.75f, 1.0f),
+        new( 0.75f, -0.75f, -0.75f, 1.0f),
+        new( -0.75f, -0.75f, -0.75f, 1.0f)
+    };
+
+    const int planeCount = 6;
+
+    var planeVertices = new int[planeCount, 4]
+    {
+        { 0, 1, 2, 3 }, // front
+        { 1, 0, 5, 4 }, // top
+        { 3, 6, 5, 0 }, // right
+        { 7, 6, 3, 2 }, // bottom
+        { 1, 4, 7, 2 }, // left
+        { 4, 5, 6, 7 } // back
+    };
+
+    theta += 0.1f * deltaTime / 16.6f;
+    if (theta > 360)
+    {
+        theta -= 360;
+    }
+
+    var transformedVertices = new Vector4[pointCount];
+
+    var rotation = Matrix4x4.CreateFromYawPitchRoll(ToRadians(-theta * 3.0f), ToRadians(theta * 2.0f), ToRadians(-theta));
+    var translation = Matrix4x4.CreateTranslation(new Vector3(0.0f, 0.0f, -5.0f));
+
+    const float fovY = 45.0f;
+    var aspect = (float)screenWidth / screenHeight;
+    const float near = 0.1f;
+    const float far = 10.0f;
+
+    var projection = Matrix4x4.CreatePerspectiveFieldOfView(ToRadians(fovY), aspect, near, far);
+    var viewFrustum = MakeViewFrustum(fovY, aspect, -near, -far);
+
+    var finalTransform = rotation * translation;
+
+    for (var i = 0; i < pointCount; i++)
+    {
+        transformedVertices[i] = Vector4.Transform(vertices[i], finalTransform);
+    }
+
+    for (var i = 0; i < planeCount; i++)
+    {
+        var vertexA = transformedVertices[planeVertices[i, 0]];
+        var vertexB = transformedVertices[planeVertices[i, 1]];
+        var vertexC = transformedVertices[planeVertices[i, 2]];
+
+        var tangent = vertexB - vertexA;
+        var biTangent = vertexC - vertexA;
+
+        var normal = Vector3.Normalize(
+            Vector3.Cross(new Vector3(tangent.X, tangent.Y, tangent.Z),
+            new Vector3(biTangent.X, biTangent.Y, biTangent.Z)));
+
+        var fragmentToViewer = new Vector3(-vertexA.X, -vertexA.Y, -vertexA.Z);
+
+        if (Vector3.Dot(normal, fragmentToViewer) < 0)
+        {
+            continue;
+        }
+
+        var edges = new EdgeTable();
+
+        for (var j = 0; j < 4; j++)
+        {
+            edges.Vertices.Add(transformedVertices[planeVertices[i, j]]);
+        }
+
+        edges = FrustumClipSimple(edges, viewFrustum);
+
+        for (var j = 0; j < edges.Vertices.Count; j++)
+        {
+            var point = Vector4.Transform(edges.Vertices[j], projection);
+
+            point.X /= point.W;
+            point.Y /= point.W;
+
+            point.X = MathF.Floor((screenWidth / 2) + (screenWidth / 2) * point.X);
+            point.Y = MathF.Floor((screenHeight / 2) + (screenHeight / 2) * point.Y);
+
+            edges.Vertices[j] = point;
+        }
+
+        var torch = new Vector3(0.0f, 0.0f, 1.0f);
+        var diffuseColor = new Color { R = 255.0f, G = 255.0f, B = 255.0f };
+        diffuseColor *= Vector3.Dot(normal, torch);
+
+        DrawPolygonFlat(diffuseColor, edges);
+    }
 }
 
 void ClipingTest()
@@ -336,6 +440,155 @@ void LinesTest()
 
     // steep, - slope
     DrawLineBresenham(new Color { B = 255.0f }, 620, 220, 32, 599);
+}
+
+void DrawPolygonFlat(Color color, EdgeTable edgeTable)
+{
+    var xStart = new int[screenWidth];
+    var xEnd = new int[screenWidth];
+
+    var yMin = int.MaxValue;
+    var yMax = 0;
+
+    foreach (var vertex in edgeTable.Vertices)
+    {
+        if (vertex.Y < yMin)
+        {
+            yMin = (int)vertex.Y;
+        }
+
+        if (vertex.Y > yMax)
+        {
+            yMax = (int)vertex.Y;
+        }
+    }
+
+    for (var y = yMin; y <= yMax; y++)
+    {
+        xStart[y] = int.MaxValue;
+        xEnd[y] = 0;
+    }
+
+    for (var i = 0; i < edgeTable.Vertices.Count; i++)
+    {
+        var x1 = edgeTable.Vertices[i].X;
+        var y1 = edgeTable.Vertices[i].Y;
+
+        var x2 = edgeTable.Vertices[(i + 1) % edgeTable.Vertices.Count].X;
+        var y2 = edgeTable.Vertices[(i + 1) % edgeTable.Vertices.Count].Y;
+
+        if (Math.Abs(x2 - x1) < Math.Abs(y2 - y1))
+        {
+            if (y1 < y2)
+            {
+                TraceSteepEdge((int)x1, (int)x2, (int)y1, (int)y2, xStart, xEnd);
+            }
+            else
+            {
+                TraceSteepEdge((int)x2, (int)x1, (int)y2, (int)y1, xStart, xEnd);
+            }
+
+            continue;
+        }
+
+        if (x1 < x2)
+        {
+            TraceShallowEdge((int)x1, (int)x2, (int)y1, (int)y2, xStart, xEnd);
+        }
+        else
+        {
+            TraceShallowEdge((int)x2, (int)x1, (int)y2, (int)y1, xStart, xEnd);
+        }
+    }
+
+    for (var y = yMin; y <= yMax; y++)
+    {
+        DrawHorizontalLine(color, xStart[y], xEnd[y], y);
+    }
+}
+
+void TraceShallowEdge(int x1, int x2, int y1, int y2, int[] xStart, int[] xEnd)
+{
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var yInc = 1;
+
+    if (dy < 0)
+    {
+        yInc = -1;
+        dy *= -1;
+    }
+
+    var d = 2 * dy - dx;
+    var dInc = 2 * (dy - dx);
+    var dNoInc = 2 * dy;
+
+    var y = y1;
+
+    for (var x = x1; x <= x2; x++)
+    {
+        if (x < xStart[y])
+        {
+            xStart[y] = x;
+        }
+
+        if (x > xEnd[y])
+        {
+            xEnd[y] = x;
+        }
+
+        if (d > 0)
+        {
+            y += yInc;
+            d += dInc;
+        }
+        else
+        {
+            d += dNoInc;
+        }
+    }
+}
+
+void TraceSteepEdge(int x1, int x2, int y1, int y2, int[] xStart, int[] xEnd)
+{
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var xInc = 1;
+
+    if (dx < 0)
+    {
+        xInc = -1;
+        dx *= -1;
+    }
+
+    var d = 2 * dx - dy;
+    var dInc = 2 * (dx - dy);
+    var dNoInc = 2 * dx;
+
+    var x = x1;
+
+    for (var y = y1; y <= y2; y++)
+    {
+        if (x < xStart[y])
+        {
+            xStart[y] = x;
+        }
+
+        if (x > xEnd[y])
+        {
+            xEnd[y] = x;
+        }
+
+        if (d > 0)
+        {
+            x += xInc;
+            d += dInc;
+        }
+        else
+        {
+            d += dNoInc;
+        }
+    }
 }
 
 EdgeTable ClipAgainstBoundry(EdgeTable input, Plane p)
